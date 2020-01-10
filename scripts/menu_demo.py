84d5fe60bd558d3c09b7a613ff75ecfcf8ebf79e
Tom Oinn <tomoinn@gmail.com>
2020-01-10 00:36:12 +0000

    Trivial reformatting

--- scripts/menu_demo.py
+++ scripts/menu_demo.py
@@ -1,10 +1,6 @@
-import logging
 from approxeng.task.menu import register_menu_tasks_from_yaml, KeyboardMenuTask
 from approxeng.task import run
 
-logging.basicConfig(level=logging.INFO)
-
-new_tasks = register_menu_tasks_from_yaml('menu_demo.yml', menu_task_class=KeyboardMenuTask)
-
-if new_tasks:
-    run(root_task=new_tasks[0])
+new_tasks = register_menu_tasks_from_yaml(filename='menu_demo.yml',
+                                          menu_task_class=KeyboardMenuTask)
+run(root_task=new_tasks[0])
